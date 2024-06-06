fa6eb15839daf4a871866c7314c19aae3a390cda
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-06-06 09:30:25 +0300

    1

---
+++ WeaponsReport/Database.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponsReport
+{
+    internal class Database
+    {
+        private List<Soldier> _soldiers = new List<Soldier>();
+
+        public Database() 
+        {
+            AddSoldiers();
+        }
+
+        private void AddSoldiers()
+        {
+            _soldiers.Add(new Soldier("Солдат1","Автомат","Сержант",12));
+            _soldiers.Add(new Soldier("Солдат2", "Винтовка", "Старший - Сержант", 10));
+            _soldiers.Add(new Soldier("Солдат3", "Пулемет", "Младший - Сержант", 5));
+            _soldiers.Add(new Soldier("Солдат4", "Гранотамет", "Рядовой", 8));
+            _soldiers.Add(new Soldier("Солдат5", "Пистолет", "Ефрейтор", 12));
+        }
+    }
+}
---
+++ WeaponsReport/Soldier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponsReport
+{
+    internal class Soldier
+    {
+        public Soldier(string name, string weapon, string militaryRank, int servicePeriod)
+        {
+            Name = name;
+            Weapon = weapon;
+            MilitaryRank = militaryRank;
+            ServicePeriod = servicePeriod;
+        }
+
+        public string Name { get; private set; }
+        public string Weapon { get; private set; }
+        public string MilitaryRank { get; private set; }
+
+        public int ServicePeriod { get; private set; }
+    }
+}
